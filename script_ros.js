1e6fe35c5b07be3522b8247cbf42ddc745dba4c2
josephgalloway321 <josephgalloway520@gmail.com>
2023-08-15 15:07:24 -0500

    Successfully controls servos from web page

--- script_ros.js
+++ script_ros.js
@@ -15,4 +15,58 @@ ros.on('close', function() {
   console.log('Connection to websocket server closed.');
 });
 
+// Create access to the DOM
+//const left_button = document.querySelector("#left-button");
 
+// Write functions to publish commands to motors
+function rotateRight() {
+  let topic = new ROSLIB.Topic({
+    ros: ros,
+    name: '/servo1',
+    messageType: 'std_msgs/UInt16'
+  });
+
+  let message = new ROSLIB.Message({
+    data: 0
+  });
+  topic.publish(message);
+}
+
+function rotateLeft() {
+  let topic = new ROSLIB.Topic({
+    ros: ros,
+    name: '/servo1',
+    messageType: 'std_msgs/UInt16'
+  });
+
+  let message = new ROSLIB.Message({
+    data: 1
+  });
+  topic.publish(message);
+}
+
+function rotateDown() {
+  let topic = new ROSLIB.Topic({
+    ros: ros,
+    name: '/servo2',
+    messageType: 'std_msgs/UInt16'
+  });
+
+  let message = new ROSLIB.Message({
+    data: 0
+  });
+  topic.publish(message);
+}
+
+function rotateUp() {
+  let topic = new ROSLIB.Topic({
+    ros: ros,
+    name: '/servo2',
+    messageType: 'std_msgs/UInt16'
+  });
+
+  let message = new ROSLIB.Message({
+    data: 1
+  });
+  topic.publish(message);
+}
