71cb421144b5e0ff9120e3d8ce0eda1b05578e4f
josephgalloway321 <josephgalloway520@gmail.com>
2023-08-16 12:55:06 -0500

    Added horizontal and vertical home buttond

--- scripts/script_ros.js
+++ scripts/script_ros.js
@@ -30,6 +30,8 @@ const right_button = document.querySelector('#right-button');
 const left_button = document.querySelector('#left-button');
 const down_button = document.querySelector('#down-button');
 const up_button = document.querySelector('#up-button');
+const horizontal_home_button = document.querySelector('#horizontal-home-button');
+const vertical_home_button = document.querySelector('#vertical-home-button');
 
 
 // Write functions to publish commands to motors
@@ -73,6 +75,33 @@ function rotateLeft() {
   }
 }
 
+function homeHorizontal() {
+  let topic = new ROSLIB.Topic({
+    ros: ros,
+    name: '/servo1',
+    messageType: 'std_msgs/UInt16'
+  });
+
+  let message = new ROSLIB.Message({
+    data: 2
+  });
+  topic.publish(message);
+
+  // Enable disabled rotate right button
+  if(right_button.disabled === true) {
+    right_button.disabled = false;
+    right_button.innerHTML = 'Rotate Right';
+    right_button.style.color = 'black';
+  }
+
+  // Enable disabled rotate left button
+  if(left_button.disabled === true) {
+    left_button.disabled = false;
+    left_button.innerHTML = 'Rotate Left';
+    left_button.style.color = 'black';
+  }
+}
+
 function rotateDown() {
   let topic = new ROSLIB.Topic({
     ros: ros,
@@ -104,6 +133,33 @@ function rotateUp() {
     data: 1
   });
   topic.publish(message);
+
+  // Enable disabled rotate up button
+  if(down_button.disabled === true) {
+    down_button.disabled = false;
+    down_button.innerHTML = 'Rotate Up';
+    down_button.style.color = 'black';
+  }
+}
+
+function homeVertical() {
+  let topic = new ROSLIB.Topic({
+    ros: ros,
+    name: '/servo2',
+    messageType: 'std_msgs/UInt16'
+  });
+
+  let message = new ROSLIB.Message({
+    data: 2
+  });
+  topic.publish(message);
+
+  // Enable disabled rotate down button
+  if(up_button.disabled === true) {
+    up_button.disabled = false;
+    up_button.innerHTML = 'Rotate Down';
+    up_button.style.color = 'black';
+  }
 
   // Enable disabled rotate up button
   if(down_button.disabled === true) {
